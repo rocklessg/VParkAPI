8fb0e75a28a01fb3df7816338c4cda47a7f04fb4
Adeyemi987 <tubosun.adeyemi@decagon.dev>
2022-10-23 20:48:22 +0100

    Creation of the Entity classes

---
+++ VPark-Models/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿using VPark_Models.Models;
+
+namespace VPark_Models
+{
+    public class Customer
+    {
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+        public ICollection<Booking> Bookings { get; set; }
+    }
+}
