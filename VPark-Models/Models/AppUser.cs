ea48eb9e6634ce29d3e0594fdb4ded868287df68
Adeyemi987 <tubosun.adeyemi@decagon.dev>
2022-10-23 21:28:16 +0100

    Modification to the AppUser Model

--- VPark-Models/Models/AppUser.cs
+++ VPark-Models/Models/AppUser.cs
@@ -11,7 +11,6 @@ namespace VPark_Models.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string FullName { get; set; }
         public string Gender { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime ModifiedAt { get; set; }
